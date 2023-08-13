3354bf19e5ce94fb4805bf6d8cf31da5f09b2b84
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-13 09:27:03 +0200

    book-list: add bg-body-secondary as background color

--- book-list/src/App.js
+++ book-list/src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="row g-0">
+    <div className="row g-0 vh-100 bg-body-secondary">
       <div className="col-md-12">
         <h1>Book List</h1>
       </div>
