c80b2f0bffcfdbe9462593367beda82ee26e7d49
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-13 09:47:24 +0200

    book-list: implement Header component

--- book-list/src/App.js
+++ book-list/src/App.js
@@ -1,8 +1,10 @@
+import Header from "components/Header"
+
 function App() {
   return (
     <div className="row g-0 vh-100 bg-body-secondary">
       <div className="col-md-12">
-        <h1>Book List</h1>
+        <Header />
       </div>
     </div>
   );
---
+++ book-list/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => (
+  <header>
+    <div className="overlay">
+      <h1>Book List</h1>
+    </div>
+  </header>
+)
+
+export default Header
