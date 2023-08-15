1396f0d9ad6db3fbbe0fee37f0d9bbf9e3fbba46
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-15 15:36:17 +0200

    book-list: add book form in BookModal

--- book-list/src/components/BookList/BookModal/BookModal.js
+++ book-list/src/components/BookList/BookModal/BookModal.js
@@ -1,19 +1,28 @@
-import Modal from 'react-bootstrap/Modal'
+import Modal from "react-bootstrap/Modal"
+import FormControl from "react-bootstrap/FormControl"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
 
 const BookModal = ({ showModal, onClose }) => {
   return (
     <Modal show={showModal} onHide={onClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Title</Modal.Title>
+        <Modal.Title>Add New Book</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-        Body
-      </Modal.Body>
+        <Form>
+          <FormControl placeholder="ISBN" className="mt-2" required />
 
-      <Modal.Footer>
-        Footer
-      </Modal.Footer>
+          <FormControl placeholder="Title" className="mt-2" required />
+
+          <Form.Select className="mt-2" required>
+            <option value="">Select Author</option>
+          </Form.Select>
+
+          <Button variant="success" className="mt-2 w-25" type="submit">Add</Button>
+        </Form>
+      </Modal.Body>
     </Modal>
   )
 }
--- book-list/src/components/BookList/BookModal/BookModal.test.js
+++ book-list/src/components/BookList/BookModal/BookModal.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import BookModal from './BookModal'
 
 test('should render BookModal component', () => {
-  const view = render(<BookModal />)
+  const view = render(<BookModal showModal={true} />)
 
   expect(view).toMatchSnapshot()
 })
