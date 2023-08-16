4be17a26f0981d4f1c9c9092211b185c91c1abc4
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-16 10:02:34 +0200

    book-list: Implement Add Book feature. The user is able to add new book to specific author

--- book-list/src/components/BookList/BookModal/BookModal.js
+++ book-list/src/components/BookList/BookModal/BookModal.js
@@ -1,4 +1,5 @@
-import { gql, useQuery } from "@apollo/client"
+import { useState } from "react"
+import { gql, useQuery, useMutation } from "@apollo/client"
 
 import Modal from "react-bootstrap/Modal"
 import FormControl from "react-bootstrap/FormControl"
@@ -14,8 +15,53 @@ const GET_AUTHORS = gql`
   }
 `
 
+const ADD_BOOK = gql`
+  mutation AddBook($isbn: String!, $title: String!, $authorId: String!){
+    addBook(isbn: $isbn, title: $title, authorId: $authorId) {
+      isbn
+    }
+  }
+  `
+
 const BookModal = ({ showModal, onClose }) => {
+  const [isbn, setIsbn] = useState("")
+  const [title, setTitle] = useState("")
+  const [authorId, setAuthorId] = useState("")
+
   const { data } = useQuery(GET_AUTHORS)
+
+  const [addBook] = useMutation(ADD_BOOK, {
+    onCompleted: () => {
+      onClose()
+    },
+    onError: () => {
+      onClose()
+    }
+  })
+
+  const handleIsbnChange = ({ target: { value } }) => {
+    setIsbn(value)
+  }
+
+  const handleTitleChange = ({ target: { value } }) => {
+    setTitle(value)
+  }
+
+  const handleAuthorChange = ({ target: { value } }) => {
+    setAuthorId(value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    addBook({
+      variables: {
+        isbn,
+        title,
+        authorId
+      }
+    })
+  }
 
   return (
     <Modal show={showModal} onHide={onClose}>
@@ -25,11 +71,28 @@ const BookModal = ({ showModal, onClose }) => {
 
       <Modal.Body>
         <Form>
-          <FormControl placeholder="ISBN" className="mt-2" required />
+          <FormControl
+            value={isbn}
+            placeholder="ISBN"
+            className="mt-2"
+            onChange={handleIsbnChange}
+            required
+          />
 
-          <FormControl placeholder="Title" className="mt-2" required />
+          <FormControl
+            value={title}
+            placeholder="Title"
+            className="mt-2"
+            onChange={handleTitleChange}
+            required
+          />
 
-          <Form.Select className="mt-2" required>
+          <Form.Select
+            value={authorId}
+            className="mt-2"
+            onChange={handleAuthorChange}
+            required
+          >
             <option value="">Select Author</option>
             {
               data?.authors.map(v => (
@@ -38,7 +101,14 @@ const BookModal = ({ showModal, onClose }) => {
             }
           </Form.Select>
 
-          <Button variant="success" className="mt-2 w-25" type="submit">Add</Button>
+          <Button
+            variant="success"
+            className="mt-2 w-25"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Add
+          </Button>
         </Form>
       </Modal.Body>
     </Modal>
